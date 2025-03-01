4759607e424f08018f1e69a3ee395eec23a0067d
nisu joshi <nisarg.joshi.95@gmail.com>
2025-03-01 03:22:22 -0500

    🔧 feat(config): enable debug mode in Nuxt configuration
    Enables debug mode in the Nuxt application to help with development and
    troubleshooting. This will provide more detailed logging and debugging information
    during development.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
+  debug: true,
+
   modules: [
     "@sentry/nuxt/module",
     "@nuxtjs/color-mode",
